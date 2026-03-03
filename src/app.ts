e6a5c84644623e609b4260157ebb790f9b258bff
Theo Björkqvist <113356799+pengabutiken@users.noreply.github.com>
2026-03-03 21:59:35 +0100

    + Fake Databas och testa databas

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-export {};
+import { getAllProducts } from "./repos/productRepo";
+
+function main() {
+    const products = getAllProducts();
+
+    console.log("Alla produkter:");
+    console.log(products);
+}
+
+main();
--- src/repos/productRepo.ts
+++ src/repos/productRepo.ts
@@ -1,1 +1,10 @@
-export {};
+import {Product} from "../models/Product";
+
+export const products: Product[]=[
+    {id:1, name:"Öl", price:69, stock: 120},
+    {id: 2, name:"Cola-Zero", price:25, stock:100},
+    {id: 3, name:"Fanta-Zero", price:25, stock:200},
+];
+export function getAllProducts(): Product[]{
+    return products;
+}
